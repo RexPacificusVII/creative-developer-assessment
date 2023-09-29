a535ca2cb03161b8e5f7cbec427380053c553bd8
RexPacificusVII <110966402+RexPacificusVII@users.noreply.github.com>
2023-09-29 14:07:42 +0800

    Fix animation timing

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 // Fade in the female character
 const femaleCharacter = document.querySelector('.female-character');
 femaleCharacter.style.opacity = 0;
-femaleCharacter.style.transition = 'opacity 0.5s ease-in';
+femaleCharacter.style.transform = 'translateX(-150%) translateY(-50%)';
+femaleCharacter.style.transition = 'opacity 1s ease-in';
 
 setTimeout(() => {
-  femaleCharacter.style.opacity = 1;
+    femaleCharacter.style.opacity = 1;
+    femaleCharacter.style.transform = 'translateX(-50%) translateY(-50%)';
 }, 1500);
 
 // Slide in the headline1 from the left
 const headline1 = document.querySelector('.headline1');
-headline1.style.transform = 'translateX(-150%) translateY(-50%)';
+headline1.style.opacity = 0;
+headline1.style.transform = 'translateX(-100%) translateY(-50%)';
+headline1.style.transition = 'opacity 0.5s ease-in, transform 0.5s ease-in';
 
 setTimeout(() => {
+    headline1.style.opacity = 1
   headline1.style.transform = 'translateX(-50%) translateY(-50%)';
 }, 2000);
 
@@ -19,54 +24,67 @@ setTimeout(() => {
 setTimeout(() => {
     femaleCharacter.style.opacity = 0;
     femaleCharacter.style.transition = 'opacity 0.5s ease-out';
-  headline1.style.transform = 'translateX(-150%) translateY(-50%)';
-}, 3000);
+    headline1.style.opacity = 0;
+    headline1.style.transform = 'translateX(-100%) translateY(-50%)';
+    headline1.style.transition = 'opacity 0.5s ease-out, transform 0.5s ease-out';
+}, 5000);
 
 // Slide in the headline2 from the right
 const headline2 = document.querySelector('.headline2');
-headline2.style.transform = 'translateX(150%) translateY(-50%)';
+headline2.style.opacity = 0;
+headline2.style.transform = 'translateX(60%) translateY(-50%)';
+headline2.style.transition = 'opacity 1.5s ease-in, transform 0.5s ease-in';
 
 setTimeout(() => {
+    headline2.style.opacity = 1;
   headline2.style.transform = 'translateX(-50%) translateY(-50%)';
-}, 3500);
+}, 5500);
 
 // Fade in the subheadline
 const subheadline = document.querySelector('.subheadline');
 subheadline.style.opacity = 0;
-femaleCharacter.style.transition = 'opacity 0.5s ease-in';
+subheadline.style.transform = 'translateX(-150%) translateY(-50%)';
+subheadline.style.transition = 'opacity 1s ease-in';
 
 setTimeout(() => {
-  subheadline.style.opacity = 1;
-}, 4000);
+    subheadline.style.opacity = 1;
+    subheadline.style.transform = 'translateX(-50%) translateY(-50%)';
+}, 6000);
 
 // Fade in the Learn More button
 const learnMoreBtn = document.querySelector('.learnmore-btn');
 learnMoreBtn.style.opacity = 0;
-femaleCharacter.style.transition = 'opacity 0.5s ease-in';
+learnMoreBtn.style.transform = 'translateX(-150%) translateY(-50%)';
+learnMoreBtn.style.transition = 'opacity 1s ease-in';
 
 setTimeout(() => {
-  learnMoreBtn.style.opacity = 1;
-}, 4500);
+    learnMoreBtn.style.opacity = 1;
+    learnMoreBtn.style.transform = 'translateX(-50%) translateY(-50%)';
+}, 6500);
 
 // Slide in the logo from the right
 const logo = document.querySelector('.logo');
-logo.style.transform = 'translateX(150%) translateY(-50%)';
+logo.style.opacity = 0;
+logo.style.transform = 'translateX(30%) translateY(-50%)';
+logo.style.transition = 'opacity 1s ease-in, transform 0.5s ease-in';
 
 setTimeout(() => {
+    logo.style.opacity = 1;
   logo.style.transform = 'translateX(-50%) translateY(-50%)';
-}, 5000);
+}, 6500);
 
 // Fade in the replay button
 const replayBtn = document.querySelector('.replay-btn');
 replayBtn.style.opacity = 0;
-femaleCharacter.style.transition = 'opacity 0.5s ease-in';
+replayBtn.style.transform = 'translateX(-150%) translateY(-50%)';
+replayBtn.style.transition = 'opacity 1s ease-in';
 
 setTimeout(() => {
-  replayBtn.style.opacity = 1;
-}, 6000);
+    replayBtn.style.opacity = 1;
+    replayBtn.style.transform = 'translateX(-50%) translateY(-50%)';
+}, 7000);
 
-// Add a click event listener to the replay button to restart the animation
+
 replayBtn.addEventListener('click', () => {
-  // Refresh the banner only
   window.location.reload('#banner');
 });
